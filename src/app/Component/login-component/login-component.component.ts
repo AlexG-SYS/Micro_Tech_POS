43b20d27edc02cce2ee7848be37e3f07d0df15c8
Alex G <aguerra@gs-com.bz>
2023-05-22 17:14:35 -0600

    Ace - Redesign Login Page

--- src/app/Component/login-component/login-component.component.ts
+++ src/app/Component/login-component/login-component.component.ts
@@ -14,7 +14,6 @@ export class LoginComponentComponent {
 
   constructor(private userService: UserService, private db: AngularFirestore, private router: Router) {
     // Sets the Global Variables to NULL
-    GlobalComponent.companyName = "";
     GlobalComponent.userName = "";
     GlobalComponent.privilege = "";
 
@@ -27,43 +26,32 @@ export class LoginComponentComponent {
   onLoginSubmit(loginData: NgForm) {
     this.clicked = true;
     // Checks input fields for empty string
-    if (loginData.value.companyName.length == 0 || loginData.value.username.length == 0 || loginData.value.password.length == 0) {
+    if (loginData.value.username.length == 0 || loginData.value.password.length == 0) {
       // If empty returns an error message
       this.error = "Empty Field"
+      loginData.resetForm();
       this.clicked = false;
+      this.myInputField.nativeElement.focus();
     }
     else {
-      GlobalComponent.companyName = loginData.value.companyName.toLowerCase();
-      // If not empty checks the database for company name
-      this.db.collection(loginData.value.companyName.toLowerCase()).get().subscribe(snaps => {
-        if (snaps.empty) {
-          // If company name does not exist return error
-          this.error = "Company File Does Not Exist"
+      // If not empty checks the database to verify username and password
+      this.userService.getUserCredentials(loginData.value.username.toLowerCase()).subscribe(userData => {
+        if (userData.length != 0 && userData[0].username == loginData.value.username.toLowerCase() && userData[0].password == loginData.value.password) {
+          // If login credentials are correct it saves the Username to global variable
+          console.log("LOGIN Successful")
+          GlobalComponent.userName = loginData.value.username.toLowerCase();
+          GlobalComponent.privilege = userData[0].privilege;
+          // Resets the form and navigates to Dashboard page
+          loginData.resetForm();
+          this.error = "";
+          this.router.navigate(['/dashboard/home']);
+        }
+        else {
+          // If login credentials are not correct then reset form and show error message
+          this.error = "Incorrect Username or Password"
           loginData.resetForm();
           this.clicked = false;
           this.myInputField.nativeElement.focus();
-        }
-        else {
-          // If company name does exist verifies username and password
-          this.userService.getUserCredentials(loginData.value.username.toLowerCase()).subscribe(userData => {
-            if (userData.length != 0 && userData[0].username == loginData.value.username.toLowerCase() && userData[0].password == loginData.value.password) {
-              // If login credentials are correct it saves the Company name and Username to global variable
-              console.log("LOGIN Successful")
-              GlobalComponent.userName = loginData.value.username.toLowerCase();
-              GlobalComponent.privilege = userData[0].privilege;
-              // Resets the form and navigates to Dashboard page
-              loginData.resetForm();
-              this.error = "";
-              this.router.navigate(['/dashboard/home']);
-            }
-            else {
-              // If login credentials are not correct then reset form and show error message
-              this.error = "Incorrect Username or Password"
-              loginData.resetForm();
-              this.clicked = false;
-              this.myInputField.nativeElement.focus();
-            }
-          })
         }
       })
     }
--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -14,22 +14,22 @@ export class UserService {
 
   constructor(private db: AngularFirestore) { }
 
-  getAllUserCredentials(){
-    return this.db.collection("/" + GlobalComponent.companyName + "/jodK1Ymec6nYUgcOhf1I-" + GlobalComponent.companyName + "/users")
-    .get().pipe(
-      map(snaps => convertSnaps<User>(snaps))
-    )
+  getAllUserCredentials() {
+    return this.db.collection("/users")
+      .get().pipe(
+        map(snaps => convertSnaps<User>(snaps))
+      )
   }
 
-  getUserCredentials(username: string){
-    return this.db.collection("/" + GlobalComponent.companyName + "/jodK1Ymec6nYUgcOhf1I-" + GlobalComponent.companyName + "/users",
-      ref => ref.where("username","==",username)
+  getUserCredentials(username: string) {
+    return this.db.collection("/users",
+      ref => ref.where("username", "==", username)
     ).get().pipe(
       map(snaps => convertSnaps<User>(snaps))
     )
   }
 
-  updateUserCredentials(userID: string, changes: Partial<User>){
-    return from(this.db.doc("/" + GlobalComponent.companyName + "/jodK1Ymec6nYUgcOhf1I-" + GlobalComponent.companyName + "/users/" + userID).update(changes));
+  updateUserCredentials(userID: string, changes: Partial<User>) {
+    return from(this.db.doc("/users/" + userID).update(changes));
   }
 }
